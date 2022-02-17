16eaf51d32800f03d41510928601fd5d77197043
Timothy Wiley <timothy.wiley@rmit.edu.au>
2022-02-17 15:21:09 +1100

    Code fixes to example rotate script

--- noetic_workspace/src/aiil_rosbot_demo/scripts/rotate.py
+++ noetic_workspace/src/aiil_rosbot_demo/scripts/rotate.py
@@ -27,7 +27,6 @@ def drive():
     rate = rospy.Rate(10)
     while (not stop) and (not rospy.is_shutdown()):
         cmd.angular.z = speed
-        rospy.loginfo(cmd)
         pub_drive.publish(cmd)
         rate.sleep()
         
